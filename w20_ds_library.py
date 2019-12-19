bdc28db68a21bece1d136e0c94bf00bde282d62c
uo-ds-nlp-w20 <59064719+uo-ds-nlp-w20@users.noreply.github.com>
2019-12-19 10:40:41 -0800

    Create w20_ds_library.py
    Added week 1 functions

---
+++ w20_ds_library.py
@@ -0,0 +1,89 @@
+import pandas as pd
+from typing import TypeVar
+dframe = TypeVar('pd.core.frame.DataFrame')
+
+def hello_ds():
+    print("Big hello to you")
+    
+#WEEK 1
+
+def euclidean_distance(vect1:list ,vect2:list) -> float:
+  assert isinstance(vect1, list), f'vect1 is not a list but a {type(vect1)}'
+  assert isinstance(vect2, list), f'vect2 is not a list but a {type(vect2)}'
+  assert len(vect1) == len(vect2), f"Mismatching length for euclidean vectors: {len(vect1)} and {len(vect2)}"
+
+  sum = 0
+  for i in range(len(vect1)):
+      sum += (vect1[i] - vect2[i])**2
+      
+  #could put assert here on result   
+  return sum**.5  # I claim that this square root is not needed in K-means - see why?
+
+def ordered_distances(target_vector:list, crowd_table:dframe, answer_column:str) -> list:
+  assert isinstance(target_vector, list), f'target_vector not a list but instead a {type(target_vector)}'
+  assert isinstance(crowd_table, pd.core.frame.DataFrame), f'crowd_table not a dataframe but instead a {type(crowd_table)}'
+  assert isinstance(answer_column, str), f'answer_column not a string but instead a {type(answer_column)}'
+  assert answer_column in crowd_table, f'{answer_column} is not a legit column in crowd_table - check case and spelling'
+
+  distance_list = []
+  for i,crow in crowd_table.iterrows():
+    c_vector = crow.drop([answer_column], axis=0).tolist()
+    d = euclidean_distance(target_vector, c_vector)
+    distance_list.append((i,d))
+  return sorted(distance_list, key=lambda pair: pair[1], reverse=False)
+
+def knn(target_vector:list, crowd_table:dframe, answer_column:str, k:int) -> int:
+  assert isinstance(target_vector, list), f'target_vector not a list but instead a {type(target_vector)}'
+  assert isinstance(crowd_table, pd.core.frame.DataFrame), f'crowd_table not a dataframe but instead a {type(crowd_table)}'
+  assert isinstance(answer_column, str), f'answer_column not a string but instead a {type(answer_column)}'
+  assert answer_column in crowd_table, f'{answer_column} is not a legit column in crowd_table - check case and spelling'
+  assert crowd_table[answer_column].isin([0,1]).all(), f"answer_column must be binary"
+  
+  #Comute sorted_crowd
+  sorted_crowd = ordered_distances(target_vector, crowd_table, answer_column)
+  #Compute top_k
+  top_k = [i for i,d in sorted_crowd[:k]]
+  #Compute opinions
+  opinions = [crowd_table.iloc[i][answer_column] for i in top_k]
+  #Compute winner
+  winner = 1 if opinions.count(1) > opinions.count(0) else 0
+  #Return winner
+  return winner
+
+def knn_tester(test_table, crowd_table, answer_column, k):
+  confusion_dictionary = {(0,0): 0, (0,1): 0, (1,0): 0, (1,1): 0}
+  correct = 0
+  for i,target_row in test_table.iterrows():
+    target_vector = target_row.drop([answer_column], axis=0).tolist()
+    crowd_answer = knn(target_vector, crowd_table, answer_column, k)
+    real_answer = target_row[answer_column]
+    confusion_dictionary[(crowd_answer, real_answer)] += 1
+  return confusion_dictionary
+
+def cm_accuracy(confusion_dictionary: dict) -> float:
+  assert isinstance(confusion_dictionary, dict), f'confusion_dictionary not a dictionary but instead a {type(confusion_dictionary)}'
+  
+  tp = confusion_dictionary[(1,1)]
+  fp = confusion_dictionary[(1,0)]
+  fn = confusion_dictionary[(0,1)]
+  tn = confusion_dictionary[(0,0)]
+  
+  return (tp+tn)/(tp+fp+fn+tn)
+
+def cm_f1(confusion_dictionary: dict) -> float:
+  assert isinstance(confusion_dictionary, dict), f'confusion_dictionary not a dictionary but instead a {type(confusion_dictionary)}'
+  
+  tp = confusion_dictionary[(1,1)]
+  fp = confusion_dictionary[(1,0)]
+  fn = confusion_dictionary[(0,1)]
+  tn = confusion_dictionary[(0,0)]
+  
+  recall = tp/(tp+fn) if (tp+fn) != 0 else 0  #Heuristic
+  precision = tp/(tp+fp) if (tp+fp) != 0 else 0  #Heuristic
+  
+  recall_div = 1/recall if recall != 0 else 0  #Heuristic
+  precision_div = 1/precision if precision != 0 else 0  #Heuristic
+  
+  f1 = 2/(recall_div+precision_div) if (recall_div+precision_div) != 0 else 0  #heuristic
+  
+  return f1
