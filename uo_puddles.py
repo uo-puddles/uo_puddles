02b19f9923bdc3ab2a08b0231745e74456e1d8c2
uo-puddles <59064719+uo-puddles@users.noreply.github.com>
2020-03-09 10:30:24 -0700

    Update uo_puddles.py
    bug fix

--- uo_puddles.py
+++ uo_puddles.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from typing import TypeVar, Callable
 dframe = TypeVar('pd.core.frame.DataFrame')
+narray = TypeVar('numpy.ndarray')
 
 def hello_ds():
     print("Big hello to you")
